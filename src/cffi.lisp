d031657e0f464caf1e24b05d898d9ef2f1c38196
Sean Bryant <sean@vitrue.com>
2012-03-08 11:30:33 -0500

    [wrapper] Move init functions to cffi.

--- src/cffi.lisp
+++ src/cffi.lisp
@@ -657,5 +657,27 @@
 (cffi:defcfun ("ev_loop_verify" ev_loop_verify) :void
   (loop :pointer))
  
+(defun ev_init (ev cb_)
+  (with-foreign-slots ((active pending priority cb) ev ev_io)
+    (setf active 0 
+          pending 0 
+          priority 0 
+          cb (get-callback cb_))))
 
+(defun ev_io_set (ev fd_ events_)
+  (with-foreign-slots ((fd events) ev ev_io)
+    (setf fd fd_ 
+          events events_)))
 
+(defun ev_timer_set (ev after_ repeat_)
+  (with-foreign-slots ((at repeat) ev ev_timer)
+    (setf at after_
+          repeat repeat_)))
+
+(defun ev_io_init (ev cb fd events) 
+  (ev_init ev cb)
+  (ev_io_set ev fd events))
+
+(defun ev_timer_init (ev cb after repeat)
+  (ev_init ev cb)
+  (ev_timer_set ev after repeat))
