3197ff520e1d2599f553242684800188aab16296
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-05-04 23:42:25 -0400

    [watcher-slot-access] Protocol for watcher slot access. Only implemented for timer watchers at the moment. + Also squashes some compiler warnings about unused vars.

--- src/ev.lisp
+++ src/ev.lisp
@@ -55,11 +55,26 @@
 (defgeneric stop-watcher (loop watcher &key keep-callback))
 (defgeneric start-watcher (loop watcher))
 (defgeneric event-dispatch (ev-loop &optional start-watchers))
+(defgeneric watcher-slot (watcher slot))
+(defgeneric set-watcher-slot (watcher slot value))
+(defsetf watcher-slot set-watcher-slot)
+
+(defmethod watcher-slot ((watcher ev-timer) slot)
+  "Get the value of the slot named by a symbol in this package
+sharing a name with the symbol `slot' from an `ev-timer' wrapper."
+  (foreign-slot-value (ev-pointer watcher)
+                      'ev_timer (find-symbol (symbol-name slot) (find-package :ev))))
+
+(defmethod set-watcher-slot ((watcher ev-timer) slot value)
+  "Set the value otherwise fetched with `watcher-slot' for an `ev-timer'"
+  (setf (foreign-slot-value (ev-pointer watcher)
+                            'ev_timer (find-symbol (symbol-name slot) (find-package :ev)))
+        value))
 
 (defmethod watcher-active-p ((watcher ev-watcher))
   (not (zerop (ev_is_active (ev-pointer watcher)))))
 
-(defmethod stop-watcher :before ((loop ev-loop) watcher &key keep-callback)
+(defmethod stop-watcher :before ((loop ev-loop) watcher &key &allow-other-keys)
   (unless (zerop (ev_is_pending (ev-pointer watcher)))
     (ev_invoke_pending (event-loop loop))))
 
@@ -131,6 +146,7 @@
 (defmethod event-dispatch :before ((loop ev-loop) &optional (start-watchers t))
   (when start-watchers
     (maphash (lambda (k v)
+               (declare (ignorable k))
                (start-watcher loop v)) *watchers*)))
 
 (defmethod start-watcher ((loop ev-loop) (watcher ev-idle))
